a2871f5b59cce7c47d6b64ad6954458564c14945
Daniel Babický <babicdan@fit.cvut.cz>
2026-04-21 22:15:09 +0200

    json library dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module babicdan.thesis {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
 
     opens babicdan.thesis to javafx.fxml;
     exports babicdan.thesis;
